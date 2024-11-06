c2531fe1bee255f59fa4850a044ffa80d8022627
Caleb Bae <baecal000@gmail.com>
2024-11-06 11:11:37 -0600

    test methods

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Inside main in todo app")
+	todos := Todos{}
+	todos.add("Learn Go")
+	todos.add("Get call from JPMC")
+	fmt.Printf("%+v\n\n", todos)
+	todos.delete(0)
+	fmt.Printf("%+v", todos)
 }
